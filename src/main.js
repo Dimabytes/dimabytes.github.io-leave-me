f05c84aeb2db09a6a97e68ff2b08167d1c44ad72
dimabytes <dimabytes@gmail.com>
2020-03-22 23:44:13 +0300

    add sidebar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import 'bootstrap'
+import $ from 'jquery'
+
+$('.toggle-bag').on('click', () => {
+  $('.cart__sidebar').toggleClass('active')
+});
+
+$('.sidebar__close').on('click', () => {
+  $('.cart__sidebar').toggleClass('active')
+});
