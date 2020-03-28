839590be6147e074d28d1674194860a11436cdbb
dimabytes <dimabytes@gmail.com>
2020-03-28 16:30:55 +0300

    add products

--- src/index/index.js
+++ src/index/index.js
@@ -1,2 +1,19 @@
 import './index.scss'
+import $ from 'jquery'
+import {slick, getSlick} from '../slick.min'
 
+$('.card-title').slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 0,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.card-title').hover(function () {
+  console.log($(this))
+    $(this).slick('slickNext')
+})
